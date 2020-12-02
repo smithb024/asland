ac56d35f347513a7505d2d38e4b8240cbed23537
Bert Smith <bertram.smith@gmail.com>
2020-12-02 15:40:30 +0000

    #9 Create selector view models

---
+++ Asland/Interfaces/ViewModels/Common/IEnumSelectorViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Asland.Interfaces.ViewModels.Common
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface which describes a component used by a user to select a enumerate value from all
+    /// possible enumeration values.
+    /// </summary>
+    /// <typeparam name="TEnum">Subject enumeration</typeparam>
+    public interface IEnumSelectorViewModel<TEnum> where TEnum : struct, IConvertible, IComparable, IFormattable
+    {
+        /// <summary>
+        /// Gets the name of the comonent.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Collection of all the enumerate selector controls within the enumeration selector.
+        /// </summary>
+        List<IEnumComponentViewModel<TEnum>> Controls { get; }
+
+        /// <summary>
+        /// Gets the action to invoke when a new enumerate is select.
+        /// </summary>
+        Action<TEnum> Action { get; }
+
+        /// <summary>
+        /// Set a new current value.
+        /// </summary>
+        /// <param name="newValue">The new value</param>
+        void Set(TEnum newValue);
+    }
+}
---
+++ Asland/ViewModels/Common/EnumComponentViewModel.cs
@@ -0,0 +1,113 @@
+﻿namespace Asland.ViewModels.Common
+{
+    using System;
+    using System.Windows.Input;
+    using Interfaces.ViewModels.Common;
+    using NynaeveLib.Commands;
+    using NynaeveLib.ViewModel;
+
+    /// <summary>
+    /// A view model for a single selectable component with the enumeration selector 
+    /// <see cref="IEnumSelectorViewModel{TEnum}"/>.
+    /// The enumeration selector will present one of these for each enumerate in the 
+    /// enumeration.
+    /// When selected, the enumeration selector will return a value equal to the
+    /// value represented by this component.
+    /// </summary>
+    /// <typeparam name="TEnum">Subject enumeration</typeparam>
+    public class EnumComponentViewModel<TEnum> : ViewModelBase, IEnumComponentViewModel<TEnum> where TEnum : struct, IConvertible, IComparable, IFormattable
+    {
+        /// <summary>
+        /// The value which is currently assigned to the enumeration selector.
+        /// </summary>
+        private TEnum currentValue;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="EnumComponentViewModel{TEnum}"/> class.
+        /// </summary>
+        /// <param name="representative">
+        /// The enumerate which this component represents.
+        /// </param>
+        /// <param name="current">
+        /// The currently selected enumerate.
+        /// </param>
+        /// <param name="action">
+        /// The action to run when this component is selected.
+        /// </param>
+        public EnumComponentViewModel(
+            TEnum representative,
+            TEnum current,
+            Action<TEnum> action)
+        {
+            this.RepresentativeValue = representative;
+            this.Name = representative.ToString();
+            this.currentValue = current;
+            this.Action = action;
+
+            this.SelectStateCommand =
+                new CommonCommand(
+                    this.Select);
+        }
+
+        /// <summary>
+        /// Gets the enumeration value within <see cref="TEnum"/> which is represented by this 
+        /// interface.
+        /// </summary>
+        public TEnum RepresentativeValue { get; }
+
+        /// <summary>
+        /// Gets the name of this value which is displayed to the user.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the currently selected value within the selector component as a whole.
+        /// </summary>
+        public TEnum CurrentValue
+        {
+            get
+            {
+                return this.currentValue;
+            }
+
+            private set
+            {
+                this.currentValue = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value which indicates whether this component is currently selected.
+        /// </summary>
+        public bool IsSelected => this.RepresentativeValue.CompareTo(this.CurrentValue) == 0;
+
+        /// <summary>
+        /// Gets the method to action when this component is selected by the user.
+        /// </summary>
+        public Action<TEnum> Action { get; }
+
+        /// <summary>
+        /// Gets the command presented to the user to allow them to select this component value.
+        /// </summary>
+        public ICommand SelectStateCommand { get; }
+
+        /// <summary>
+        /// Set the current value as defined within the enumeration selector component.
+        /// </summary>
+        /// <param name="newValue">new value</param>
+        public void SetCurrent(TEnum newValue)
+        {
+            this.currentValue = newValue;
+            this.RaisePropertyChangedEvent(nameof(this.IsSelected));
+        }
+
+        /// <summary>
+        /// Select this <see cref="RepresentativeValue"/> as the current value within the 
+        /// enumeration selector.
+        /// </summary>
+        private void Select()
+        {
+            this.Action.Invoke(this.RepresentativeValue);
+        }
+    }
+}
