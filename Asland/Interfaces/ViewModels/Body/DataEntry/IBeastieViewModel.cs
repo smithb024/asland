91a8cc29c35a133d1faf98d3c6480e7d2f6dd719
Bert Smith <bertram.smith@gmail.com>
2020-12-13 22:57:01 +0000

    #6 Define the beastie view model interfaces.

---
+++ Asland/Interfaces/ViewModels/Body/DataEntry/IBeastieViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Asland.Interfaces.ViewModels.Body.DataEntry
+{
+    /// <summary>
+    /// Interface for a view model which is used to select a beastie in an event.
+    /// </summary>
+    public interface IBeastieViewModel
+    {
+        /// <summary>
+        /// Gets the name of the beastie.
+        /// </summary>
+        string CommonName { get; }
+
+        /// <summary>
+        /// Gets the (latin) name of the beastie.
+        /// </summary>
+        string LatinName { get; }
+
+        /// <summary>
+        /// Gets the path to an image of the beastie.
+        /// </summary>
+        string ImagePath { get; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the beastie has been selected.
+        /// </summary>
+        bool IsSelected { get; set; }
+    }
+}
