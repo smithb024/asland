310d37dad8798a2e5f97d98dc9850d5ffa462fe9
Bert Smith <bertram.smith@gmail.com>
2020-12-14 18:35:35 +0000

    #6: Define interface for event details view model

--- Asland/Interfaces/ViewModels/Body/DataEntry/IEventDetailsEntry.cs
+++ Asland/Interfaces/ViewModels/Body/DataEntry/IEventDetailsEntry.cs
@@ -1,10 +1,54 @@
 ﻿namespace Asland.Interfaces.ViewModels.Body.DataEntry
 {
+    using System;
+    using Asland.Common.Enums;
+    using Interfaces.ViewModels.Common;
+
     /// <summary>
     /// Interface for a view model which supports a view reposible for the entry of the event 
     /// details.
     /// </summary>
     public interface IEventDetailsEntry
     {
+        /// <summary>
+        /// Gets or sets the location of the event.
+        /// </summary>
+        string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of the event.
+        /// </summary>
+        DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets any notes pertaining to the event.
+        /// </summary>
+        string Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the observations currently being marked have 
+        /// been seen.
+        /// </summary>
+        bool IsSeen { get; set; }
+
+        /// <summary>
+        /// Gets the length selector.
+        /// </summary>
+        IEnumSelectorViewModel<ObservationLength> LengthSelector { get; }
+
+        /// <summary>
+        /// Gets the intensity selector.
+        /// </summary>
+        IEnumSelectorViewModel<ObservationIntensity> IntensitySelector { get; }
+
+        /// <summary>
+        /// Gets the time of day selector.
+        /// </summary>
+        IEnumSelectorViewModel<ObservationTimeOfDay> TimeOfDatSelector { get; }
+
+        /// <summary>
+        /// Gets the weather selector.
+        /// </summary>
+        IEnumSelectorViewModel<ObservationWeather> WeatherSelector { get; }
     }
 }
--- Asland/ViewModels/Body/DataEntry/EventDetailsEntryViewModel.cs
+++ Asland/ViewModels/Body/DataEntry/EventDetailsEntryViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace Asland.ViewModels.Body.DataEntry
 {
+    using System;
+    using Asland.Common.Enums;
     using Asland.Interfaces.ViewModels.Body.DataEntry;
+    using Interfaces.ViewModels.Common;
     using NynaeveLib.ViewModel;
 
     /// <summary>
@@ -8,5 +11,53 @@
     /// </summary>
     public class EventDetailsEntryViewModel : ViewModelBase, IEventDetailsEntry
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="EventDetailsEntryViewModel"/> class.
+        /// </summary>
+        public EventDetailsEntryViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the location of the event.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of the event.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets any notes pertaining to the event.
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the observations currently being marked have 
+        /// been seen.
+        /// </summary>
+        public bool IsSeen { get; set; }
+
+        /// <summary>
+        /// Gets the length selector.
+        /// </summary>
+        public IEnumSelectorViewModel<ObservationLength> LengthSelector { get; }
+
+        /// <summary>
+        /// Gets the intensity selector.
+        /// </summary>
+        public IEnumSelectorViewModel<ObservationIntensity> IntensitySelector { get; }
+
+        /// <summary>
+        /// Gets the time of day selector.
+        /// </summary>
+        public IEnumSelectorViewModel<ObservationTimeOfDay> TimeOfDatSelector { get; }
+
+        /// <summary>
+        /// Gets the weather selector.
+        /// </summary>
+        public IEnumSelectorViewModel<ObservationWeather> WeatherSelector { get; }
     }
 }
