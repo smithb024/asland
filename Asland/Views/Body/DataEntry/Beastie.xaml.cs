c78d4b0a3f2c0c2ea93ceec64a2a931da100302a
Bert Smith <bertram.smith@gmail.com>
2020-12-27 23:40:40 +0000

    #6 Create beastie entry views.

---
+++ Asland/Views/Body/DataEntry/Beastie.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Asland.Views.Body.DataEntry
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for Beastie.xaml
+    /// </summary>
+    public partial class Beastie : UserControl
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Beastie"/> class.
+        /// </summary>
+        public Beastie()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
