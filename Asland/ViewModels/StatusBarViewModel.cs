8615f4a3580e99b90bad5b1f2ba87cd7391d091b
Bert Smith <bertram.smith@gmail.com>
2021-03-07 19:08:37 +0000

    #26: Status bar view model

---
+++ Asland/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Asland.ViewModels
+{
+    using Asland.Interfaces.ViewModels;
+
+    /// <summary>
+    /// View model which describes the application status bar.
+    /// </summary>
+    public class StatusBarViewModel : IStatusBarViewModel
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="StatusBarViewModel"/> class.
+        /// </summary>
+        public StatusBarViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the latest status of the application.
+        /// </summary>
+        public string Status { get; }
+    }
+}
