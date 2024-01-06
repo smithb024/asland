b233f1621f4e64bea5424728e5eac476a3e105eb
Bert Smith <bertram.smith@gmail.com>
2024-01-06 16:10:08 +0000

    #70: Add year tab

---
+++ Asland/Interfaces/ViewModels/Body/Analysis/IYearViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Asland.Interfaces.ViewModels.Body.Analysis
+{
+    /// <summary>
+    /// Interface which supports the year view on the analysis tab.
+    /// </summary>
+    public interface IYearViewModel
+    {
+    }
+}
