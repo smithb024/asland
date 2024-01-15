f2e2eef3ae113de2f3b7575b9460473239ee38ba
Bert Smith <bertram.smith@gmail.com>
2024-01-15 16:31:58 +0000

    #72: Add selection flags.

--- Asland/Interfaces/ViewModels/Body/Analysis/ILocationViewModel.cs
+++ Asland/Interfaces/ViewModels/Body/Analysis/ILocationViewModel.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.ObjectModel;
-
-namespace Asland.Interfaces.ViewModels.Body.Analysis
+﻿namespace Asland.Interfaces.ViewModels.Body.Analysis
 {
+    using System.Collections.ObjectModel;
+
     /// <summary>
     /// Interface which supports the beastie view on the analysis tab.
     /// </summary>
@@ -16,5 +16,25 @@ namespace Asland.Interfaces.ViewModels.Body.Analysis
         /// Gets or sets the index of the currently selected location.
         /// </summary>
         int LocationIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show NT locations.
+        /// </summary>
+        bool IsNt { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show WT locations.
+        /// </summary>
+        bool IsWt { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show WWT locations.
+        /// </summary>
+        bool IsWwt { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show RSPB locations.
+        /// </summary>
+        bool IsRspb { get; set; }
     }
 }
--- Asland/ViewModels/Body/Analysis/LocationViewModel.cs
+++ Asland/ViewModels/Body/Analysis/LocationViewModel.cs
@@ -3,10 +3,10 @@
     using Asland.Factories.IO;
     using Asland.Interfaces.ViewModels.Body.Analysis;
     using Asland.Model.IO;
-    using Asland.ViewModels.Common;
     using NynaeveLib.Logger;
     using NynaeveLib.ViewModel;
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.IO;
 
@@ -29,6 +29,26 @@
         /// The index of the currently selected location.
         /// </summary>
         private int selectedLocationIndex;
+
+        /// <summary>
+        /// Show the NT locations.
+        /// </summary>
+        private bool isNt;
+
+        /// <summary>
+        /// Show the WT locations.
+        /// </summary>
+        private bool isWt;
+
+        /// <summary>
+        /// Show the WWT locations.
+        /// </summary>
+        private bool isWwt;
+
+        /// <summary>
+        /// Show the RSPB locations.
+        /// </summary>
+        private bool isRspb;
 
         /// <summary>
         /// Initialises a new instance of the <see cref="LocationViewModel"/> class.
@@ -77,8 +97,8 @@
         /// <summary>
         /// Gets or sets the index of the currently selected location.
         /// </summary>
-        public int LocationIndex 
-        { 
+        public int LocationIndex
+        {
             get => this.selectedLocationIndex;
             set
             {
@@ -89,6 +109,126 @@
 
                 this.selectedLocationIndex = value;
                 this.RaisePropertyChangedEvent(nameof(this.LocationIndex));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show NT locations.
+        /// </summary>
+        public bool IsNt
+        {
+            get => this.isNt;
+            set
+            {
+                if (this.isNt == value)
+                {
+                    return;
+                }
+
+                this.isNt = value;
+
+                if (this.isNt == true)
+                {
+                    this.isWt = false;
+                    this.isWwt = false;
+                    this.isRspb = false;
+
+                    this.RaisePropertyChangedEvent(nameof(this.IsWt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsWwt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsRspb));
+                }
+
+                this.RaisePropertyChangedEvent(nameof(this.IsNt));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show WT locations.
+        /// </summary>
+        public bool IsWt
+        {
+            get => this.isWt;
+            set
+            {
+                if (this.isWt == value)
+                {
+                    return;
+                }
+
+                this.isWt = value;
+
+                if (this.isWt == true)
+                {
+                    this.isNt = false;
+                    this.isWwt = false;
+                    this.isRspb = false;
+
+                    this.RaisePropertyChangedEvent(nameof(this.IsNt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsWwt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsRspb));
+                }
+
+                this.RaisePropertyChangedEvent(nameof(this.IsWt));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show WWT locations.
+        /// </summary>
+        public bool IsWwt
+        {
+            get => this.isWwt;
+            set
+            {
+                if (this.isWwt == value)
+                {
+                    return;
+                }
+
+                this.isWwt = value;
+
+                if (this.isWwt == true)
+                {
+                    this.isWt = false;
+                    this.isNt = false;
+                    this.isRspb = false;
+
+                    this.RaisePropertyChangedEvent(nameof(this.IsWt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsNt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsRspb));
+                }
+
+                this.RaisePropertyChangedEvent(nameof(this.IsWwt));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to just show RSPB locations.
+        /// </summary>
+        public bool IsRspb
+        {
+            get => this.isRspb;
+            set
+            {
+                if (this.isRspb == value)
+                {
+                    return;
+                }
+
+                this.isRspb = value;
+
+                if (this.isRspb == true)
+                {
+                    this.isWt = false;
+                    this.isWwt = false;
+                    this.isNt = false;
+
+                    this.RaisePropertyChangedEvent(nameof(this.IsWt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsWwt));
+                    this.RaisePropertyChangedEvent(nameof(this.IsNt));
+                }
+
+                this.RaisePropertyChangedEvent(nameof(this.IsRspb));
             }
         }
     }
