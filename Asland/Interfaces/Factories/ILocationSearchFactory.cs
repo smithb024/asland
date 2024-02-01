6c26bee107edfa35584f0da8599399fd88d777ac
Bert Smith <bertram.smith@gmail.com>
2024-02-01 22:04:11 +0000

    #74: Introduce location interface

---
+++ Asland/Interfaces/Factories/ILocationSearchFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Asland.Interfaces.Factories
+{
+    /// <summary>
+    /// Interface used to describe the object with manages search access to the locations.
+    /// </summary>
+    public interface ILocationSearchFactory
+    {
+        /// <summary>
+        /// Find and return data for a specific location.
+        /// </summary>
+        /// <param name="name">name to search for</param>
+        void Find(string name);
+    }
+}
