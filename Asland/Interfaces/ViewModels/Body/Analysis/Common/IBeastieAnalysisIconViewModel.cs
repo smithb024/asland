0b607845a37e8e423ad573766fed729fb751755d
Bert Smith <bertram.smith@gmail.com>
2024-02-27 23:14:43 +0000

    #78: Create icon

---
+++ Asland/Interfaces/ViewModels/Body/Analysis/Common/IBeastieAnalysisIconViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Asland.Interfaces.ViewModels.Body.Analysis.Common
+{
+    public interface IBeastieAnalysisIconViewModel
+    {
+        // Count
+        // Total
+        // Percentage
+    }
+}
