87b65a2746b26c5bb17d69f146d8af52b4049807
Bert Smith <bertram.smith@gmail.com>
2024-03-04 17:51:34 +0000

    #78: Add basic icon

---
+++ Asland/Views/Body/Analysis/Common/BeastieAnalysisIcon.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Asland.Views.Body.Analysis.Common
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for BeastieAnalysisIcon.xaml
+    /// </summary>
+    public partial class BeastieAnalysisIcon : UserControl
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="BeastieAnalysisIcon"/> class.
+        /// </summary>
+        public BeastieAnalysisIcon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
