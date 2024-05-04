ebae68030b52a901a8f662f3d9e10d4243b2d023
Bert Smith <bertramsmith123@hotmail.com>
2024-05-04 22:48:21 +0100

    #58: Read file

--- Asland/PathManager.cs
+++ Asland/PathManager.cs
@@ -1,14 +1,45 @@
 ﻿namespace Asland
 {
     using Asland.Interfaces;
+    using System.Collections.Generic;
+    using System.IO;
+    using System;
 
     /// <summary>
     /// The Manager class which manages the path data.
     /// </summary>
     public class PathManager : IPathManager
     {
-        public PathManager()
+        /// <summary>
+        /// The name of the file which contains the data's base path.
+        /// </summary>
+        private string pathFileName = ".\\aslandPath.txt";
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="PathManager"/> class.
+        /// </summary>
+        /// <param name="logger">The program logger</param>
+        public PathManager(
+            IAsLogger logger)
         {
+            try
+            {
+                List<string> fileContents = new List<string>();
+
+                if (File.Exists(pathFileName))
+                {
+                    using (StreamReader reader = new StreamReader(pathFileName))
+                    {
+                        string currentLine = reader.ReadLine();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.WriteLine(
+                    string.Format("Error, failed to read {0}: {1}", pathFileName, ex.ToString()));
+            }
         }
     }
 }
