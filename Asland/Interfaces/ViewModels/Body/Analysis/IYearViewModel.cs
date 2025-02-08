f37704c953f1c46301e2ab061bc386bc63b11fb2
smithb024 <bertram.smith@gmail.com>
2025-02-08 21:55:22 +0000

    #100: Create the year combo box

--- Asland/Interfaces/ViewModels/Body/Analysis/IYearViewModel.cs
+++ Asland/Interfaces/ViewModels/Body/Analysis/IYearViewModel.cs
@@ -1,12 +1,23 @@
 ﻿namespace Asland.Interfaces.ViewModels.Body.Analysis
 {
     using Asland.Interfaces.ViewModels.Body.Analysis.Year;
+    using System.Collections.ObjectModel;
 
     /// <summary>
     /// Interface which supports the year view on the analysis tab.
     /// </summary>
     public interface IYearViewModel
     {
+        /// <summary>
+        /// Gets a collection of years.
+        /// </summary>
+        ObservableCollection<string> Years { get; }
+
+        /// <summary>
+        /// Gets or sets the index of the currently selected year.
+        /// </summary>
+        int YearIndex { get; set; }
+
         /// <summary>
         /// Gets the summary for the selected location.
         /// </summary>
--- Asland/ViewModels/Body/Analysis/YearViewModel.cs
+++ Asland/ViewModels/Body/Analysis/YearViewModel.cs
@@ -4,6 +4,7 @@
     using Asland.Interfaces.ViewModels.Body.Analysis.Year;
     using Asland.ViewModels.Body.Analysis.Year;
     using NynaeveLib.ViewModel;
+    using System.Collections.ObjectModel;
 
     /// <summary>
     /// View Model which supports the year view on the analysis tab.
@@ -11,12 +12,48 @@
     public class YearViewModel : ViewModelBase, IYearViewModel
     {
         /// <summary>
+        /// The index of the currently selected year.
+        /// </summary>
+        private int selectedYearIndex;
+
+        /// <summary>
         /// Initialises a new instance of the <see cref="YearViewModel"/> class.
         /// </summary>
         public YearViewModel() 
         {
             this.Summary =
                 new YearSummaryViewModel();
+
+            this.Years = 
+                new ObservableCollection<string>()
+                {
+                    "1",
+                    "2"
+                };
+            this.selectedYearIndex = -1;
+        }
+
+        /// <summary>
+        /// Gets a collection of years.
+        /// </summary>
+        public ObservableCollection<string> Years { get; }
+
+        /// <summary>
+        /// Gets or sets the index of the currently selected year.
+        /// </summary>
+        public int YearIndex
+        {
+            get => this.selectedYearIndex;
+            set
+            {
+                if (this.selectedYearIndex == value)
+                {
+                    return;
+                }
+
+                this.selectedYearIndex = value;
+                this.OnPropertyChanged(nameof(this.YearIndex));
+            }
         }
 
         /// <summary>
