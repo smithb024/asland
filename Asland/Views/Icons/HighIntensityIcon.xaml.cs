b1350e56018ebdf1811a8124336315a4a51f4343
smithb024 <bertram.smith@gmail.com>
2025-07-01 23:01:42 +0100

    #110: Add high intensity icon

---
+++ Asland/Views/Icons/HighIntensityIcon.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Asland.Views.Icons
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for HighIntensityIcon.xaml
+    /// </summary>
+    public partial class HighIntensityIcon : UserControl
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="HighIntensityIcon"/> class.
+        /// </summary>
+        public HighIntensityIcon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
