58531e25d18438921428952629beb257e7c4cab8
smithb024 <bertram.smith@gmail.com>
2025-07-03 22:31:28 +0100

    #110: Create the dependency properties

--- Asland/Views/Icons/HighIntensityIcon.xaml.cs
+++ Asland/Views/Icons/HighIntensityIcon.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace Asland.Views.Icons
 {
+    using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for HighIntensityIcon.xaml
@@ -8,11 +10,30 @@
     public partial class HighIntensityIcon : UserControl
     {
         /// <summary>
+        /// Used to paint this icon
+        /// </summary>
+        public static readonly DependencyProperty HighIntensityBrushProperty =
+            DependencyProperty.Register(
+              nameof(HighIntensityBrush),
+              typeof(Brush),
+              typeof(HighIntensityIcon),
+              new PropertyMetadata(new SolidColorBrush(Colors.HotPink)));
+
+        /// <summary>
         /// Initialises a new instance of the <see cref="HighIntensityIcon"/> class.
         /// </summary>
         public HighIntensityIcon()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the brush used to paint the icon.
+        /// </summary>
+        public Brush HighIntensityBrush
+        {
+            get => (Brush)this.GetValue(HighIntensityBrushProperty);
+            set => this.SetValue(HighIntensityBrushProperty, value);
         }
     }
 }
