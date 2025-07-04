81c48f263788d736341717f7d3aaac97028a62bb
smithb024 <bertram.smith@gmail.com>
2025-07-04 22:51:56 +0100

    #110: Create Medium intensity icon

---
+++ Asland/Views/Icons/MediumIntensityIcon.xaml.cs
@@ -0,0 +1,60 @@
+﻿namespace Asland.Views.Icons
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for MediumIntensityIcon.xaml
+    /// </summary>
+    public partial class MediumIntensityIcon : UserControl
+    {
+        /// <summary>
+        /// Used to paint this icon
+        /// </summary>
+        public static readonly DependencyProperty MediumIntensityBrushProperty =
+            DependencyProperty.Register(
+              nameof(MediumIntensityBrush),
+              typeof(Brush),
+              typeof(MediumIntensityIcon),
+              new PropertyMetadata(
+                  new SolidColorBrush(Colors.HotPink),
+                  OnMediumIntensityPropertyChanged)); 
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MediumIntensityIcon"/> class.
+        /// </summary>
+        public MediumIntensityIcon()
+        {
+            this.InitializeComponent();
+            this.MediumIntensityPath.Fill = this.MediumIntensityBrush;
+            this.MediumIntensityPath.Stroke = this.MediumIntensityBrush;
+        }
+
+        /// <summary>
+        /// Gets or sets the brush used to paint the icon.
+        /// </summary>
+        public Brush MediumIntensityBrush
+        {
+            get => (Brush)this.GetValue(MediumIntensityBrushProperty);
+            set => this.SetValue(MediumIntensityBrushProperty, value);
+        }
+
+        /// <summary>
+        /// The brush changed event
+        /// </summary>
+        /// <param name="d">The sender</param>
+        /// <param name="e">The event argument</param>
+        private static void OnMediumIntensityPropertyChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MediumIntensityIcon mediumIntensity)
+            {
+                Brush newBrush = e.NewValue as Brush;
+                mediumIntensity.MediumIntensityPath.Fill = newBrush;
+                mediumIntensity.MediumIntensityPath.Stroke = newBrush;
+            }
+        }
+    }
+}
