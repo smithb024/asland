3be15a1d3411cfa7345b7e65083794dd91a9fe2a
smithb024 <bertram.smith@gmail.com>
2025-08-26 22:30:09 +0100

    #114: Intensity colours

---
+++ Asland/Common/Utils/ColourLibrary.cs
@@ -0,0 +1,65 @@
+﻿namespace Asland.Common.Utils
+{
+    using System.Windows.Media;
+
+    /// <summary>
+    /// A static class used to store all colours used by the c# code within the solution.
+    /// </summary>
+    public static class ColourLibrary
+    {
+        /// <summary>
+        /// A colour which is used for an unrecognised colour.
+        /// </summary>
+        public static Color UnknownColour => Colors.HotPink;
+
+        /// <summary>
+        /// A colour which is used for a snapshot.
+        /// </summary>
+        public static Color Snapshot => Colors.Indigo;
+
+        /// <summary>
+        /// A colour which is used for the high intensity.
+        /// </summary>
+        public static Color HighIntensity => Colors.DarkGreen;
+
+        /// <summary>
+        /// A colour which is used for the medium intensity.
+        /// </summary>
+        public static Color MediumIntensity => Colors.ForestGreen;
+
+        /// <summary>
+        /// A colour which used for the low intensity.
+        /// </summary>
+        public static Color LowIntensity => Colors.Olive;
+
+        /// <summary>
+        /// A colour which is used for a run.
+        /// </summary>
+        public static Color Run => Colors.DarkBlue;
+
+        /// <summary>
+        /// A colour which is used for a ride.
+        /// </summary>
+        public static Color Cycling => Colors.MediumBlue;
+
+        /// <summary>
+        /// A colour which is used for a walk.
+        /// </summary>
+        public static Color Walk => Colors.RoyalBlue;
+
+        /// <summary>
+        /// A colour which is used for the commute.
+        /// </summary>
+        public static Color Commute => Colors.IndianRed;
+
+        /// <summary>
+        /// A colour which is used for rail.
+        /// </summary>
+        public static Color Rail => Colors.DarkGoldenrod;
+
+        /// <summary>
+        /// A colour which is used for sea.
+        /// </summary>
+        public static Color Sea => Colors.Goldenrod;
+    }
+}
